7186567f950cc41853bbc53f44f8f9cc9cde2905
Sudeep <sudeepsavai16@gmail.com>
2023-06-17 16:05:29 +0530

    modified for d

--- Musicportal/settings.py
+++ Musicportal/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'Musicportal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'OVcvwNrbAN2XhiHY8ZXt',
+#         'HOST': 'containers-us-west-22.railway.app',
+#         'PORT': '5621',
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'OVcvwNrbAN2XhiHY8ZXt',
-        'HOST': 'containers-us-west-22.railway.app',
-        'PORT': '5621',
-    }
-}
 
 
 
